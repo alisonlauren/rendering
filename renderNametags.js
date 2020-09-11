c1cd675346217212c43ce009f54a6404f01e595d
Alison Manning <alisonlaurenmanning@gmail.com>
2020-09-10 19:52:58 -0500

    add

--- renderNametags.js
+++ renderNametags.js
@@ -1,8 +1,18 @@
-
 function renderNametags(nametags) {
+
+
+    var nametagsHTML = nametags.map(function(currentName){
+        return ` 
+            <div class="nametag mx-auto w-50 text-center border m-1">
+                <div class="hello bg-primary text-white p-3 h3">Hello, my name is:</div>
+                <div class="name h4 p-5">${currentName}</div>
+            </div>
+        `;
+    });   
+
     return `
-        <div class="text-center mt-5">
-            <code>${JSON.stringify(nametags)}</code>
+        <div class="nametagContainer">
+            ${nametagsHTML.join('')}           
         </div>
     `
 }
